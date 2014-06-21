f21c42b774f4386ecf33a5758b4d6bd92fc5d49f
agchou <aarongchou@gmail.com>
2014-06-21 14:07:46 -0700

    removes require for cookieParser

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var twitterService = require('./actions/twitter');
